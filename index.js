e6ea9ffc31361745ce7b69981e58bdc9216eda91
Jake <jacobtlanghel@gmail.com>
2022-05-22 15:54:45 -0400

    add first media query

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const openMenu = () => {
   document.querySelector(".nav-list").style.display = "block";
   document.querySelector(".menu-btn-open").style.display = "none";
   document.querySelector(".menu-btn-close").style.display = "block";
-  //   document.querySelector("main").classList.add("nav-open");
 };
 
 const closeNav = () => {
   document.querySelector(".nav-list").style.display = "none";
   document.querySelector(".menu-btn-close").style.display = "none";
   document.querySelector(".menu-btn-open").style.display = "block";
-  //   document.querySelector("main").classList.remove("nav-open");
 };
 
 openMenuBtn.addEventListener("click", openMenu);
